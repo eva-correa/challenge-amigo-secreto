69b8b80806dea80711e9378df9a52644656bafa5
eva-correa <darosanunes@gmail.com>
2025-08-22 18:41:12 -0300

    Função para inserir e mostrar amigos na tela

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaamigos=[];
+
+
+
+
+
+// função que vai receber o nome dos amigos e armazenar em uma lista.
+function adicionarAmigo(){
+    let nome=  document.querySelector('input').value;
+    if ( nome ==''){
+        alert("Por favor insira um nome válido!"); 
+    } else {
+        listaamigos.push (nome);
+        console.log (listaamigos);
+      }
+    mostrarAmigos();
+    limparCampo();
+}
+
+
+//função para mostrar os amigos inseridos no HTML.
+function mostrarAmigos(){
+    // Seleciona o elemento <ul> pelo ID
+    const listaElemento = document.getElementById("listaAmigos");
+        listaElemento.innerHTML = "";
+    // Percorre o array e adiciona cada item à lista
+   listaamigos.forEach(itemTexto => {
+        // 1. Cria um novo elemento de item de lista (<li>)
+        const novoItem = document.createElement("li");
+
+        // 2. Define o texto do item de lista
+        novoItem.textContent = itemTexto;
+
+        // 3. Adiciona o novo item à lista <ul>
+        listaElemento.appendChild(novoItem);
+     });
+
+}
+
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
